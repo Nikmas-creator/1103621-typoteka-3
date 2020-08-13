848baaf6051ca6216ea2a17ed68f3df0aa7d0a71
Nikita Maslov <51367582+Nikmas-creator@users.noreply.github.com>
2020-08-13 17:43:46 +0300

    module3-task1 is completed

---
+++ src/express/routes/main-routes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const {Router} = require(`express`);
+const mainRouter = new Router();
+
+mainRouter.get(`/`, (req, res) => res.send(`/`));
+mainRouter.get(`/login`, (req, res) => res.send(`/login`));
+mainRouter.get(`/my`, (req, res) => res.send(`/my`));
+mainRouter.get(`/search`, (req, res) => res.send(`/search`));
+mainRouter.get(`/categories`, (req, res) => res.send(`/categories`));
+mainRouter.get(`/register`, (req, res) => res.send(`/register`));
+
+module.exports = mainRouter;
