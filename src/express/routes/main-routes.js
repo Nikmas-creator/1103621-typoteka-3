329c38bdbb656984ba46d819c3845760d3c94f1f
Nikita Maslov <51367582+Nikmas-creator@users.noreply.github.com>
2020-08-17 12:46:48 +0300

    added links into templates and set up 404 and 500 pages

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -4,9 +4,9 @@ const {Router} = require(`express`);
 const mainRouter = new Router();
 
 mainRouter.get(`/`, (req, res) => res.render(`main`));
-mainRouter.get(`/login`, (req, res) => res.render(`login`));
+mainRouter.get(`/login`, (req, res) => res.render(`entry/login`));
 mainRouter.get(`/search`, (req, res) => res.render(`search`));
 mainRouter.get(`/categories`, (req, res) => res.render(`all-categories`));
-mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
+mainRouter.get(`/register`, (req, res) => res.render(`entry/sign-up`));
 
 module.exports = mainRouter;
